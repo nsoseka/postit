67a29f537b2313c53d5f0f5adb5097eff6aa5afe
Agape <lisaacjunior@yahoo.com>
2017-11-05 23:01:00 +0100

    ''

--- db/migrate/20171005195024_add_category_id_to_posts.rb
+++ db/migrate/20171005195024_add_category_id_to_posts.rb
@@ -1,6 +1,6 @@
 class AddCategoryIdToPosts < ActiveRecord::Migration
   def change
   	add_column :posts, :category_id, :integer
-  	remove_column :categories, :category_id, :integer
+  	#remove_column :categories, :category_id, :integer
   end
 end
